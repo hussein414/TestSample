43ab5a0c16aaf85c4a75f1b91cdb187b4fc044cd
hamed.taghavi@gmail.com <hamed.taghavi@gmail.com>
2023-08-14 21:26:10 -0400

    sample tunnel

--- app/src/main/java/com/example/testsample/ui/view/activites/MainActivity.kt
+++ app/src/main/java/com/example/testsample/ui/view/activites/MainActivity.kt
@@ -23,10 +23,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         setStatusBar()
 
-
-
         setViewModel()
-
     }
 
     private fun setStatusBar() {
---
+++ app/src/main/java/com/example/testsample/vpnclient/vpn/Constants.kt
@@ -0,0 +1,4 @@
+package com.fanoos.vpnclient.vpn
+
+
+var isMyVpnServiceRunning = false
